ec07a434253e0508ded9c41f82d8336f08dabb75
Miguel Penha <miguel.penha1@gmail.com>
2023-12-12 09:42:41 -0300

    Teste

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,16 @@ import getRepositories from './getRepositories'
 import config from './config.json'
 import command from './command'
 import { blueBright as info, greenBright as success, bold } from 'chalk'
+import path from 'path'
 
 if (process.argv[2] === 'config') {
     openConfig()
 } else {
-    const repositories = getRepositories(process.argv[2] || config.DIST_SRC)
+    const repositories = getRepositories((process.argv[2] || config.DIST_SRC).replace('"', ''))
 
     console.log(info('>> Folder'))
-    console.log(info(`  >> ${process.argv[2] || config.DIST_SRC}`))
+    console.log(info(`  >> Input Path "${(process.argv[2] || config.DIST_SRC).replace('"', '')}"`))
+    console.log(info(`  >> Complete Path "${path.resolve((process.argv[2] || config.DIST_SRC).replace('"', ''))}"`))
 
     repositories.map(repository => 
         command(`cd ${repository.path} && git fetch`, () => 
